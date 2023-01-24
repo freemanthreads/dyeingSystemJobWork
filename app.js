fddad691faa433a74b951987dab911abeaa501db
Giovanni Georgio <navmeetsekhon@gmail.com>
2023-01-24 10:24:26 +0530

    node js server files added now

---
+++ app.js
@@ -0,0 +1,31 @@
+const mysql=require('mysql');
+const express =require('express');
+const app=express();
+
+const db=mysql.createConnection({
+    host:'localhost',
+    port:'3306',
+    user:'root',
+    password:'password',
+    database:'jobworkdb'
+});
+db.connect(function(err){
+    if(err){
+        console.log("database connectivity error"+err.stack);
+        return;
+    }
+    else{
+        console.log("database connectivity succesfull");
+    }
+});
+app.get('/challanList',(req,res)=>{
+    sql='SELECT * FROM ch_main;'
+    db.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+    res.end;
+})
+app.listen(8080, () => {
+    console.log(`Server running `);
+});
